c5bd7ace706b826a68847028c9ef37d5a0c32b4b
Caroline Rozali <UnorthodoxThing@Outlook.com>
2019-03-15 17:53:26 +1100

    fix typo in script.js

--- script.js
+++ script.js
@@ -27,17 +27,36 @@ SOUND_TRACKS.push(airHornTrack, clownHornTrack, fartTootTrack, nikkoNikkoNiiTrac
 
 let containerRoom = document.querySelector('.container');
 
-// Check if key press is equal to the following keys in KEY_CODES
-function checkKeyCode(pressAnyKey) {
-  for (let aKeyCode of KEY_CODES) {
-    if (pressAnyKey === KEY_CODES) {
-      // what is the key?
-      return console.log("True!");
-    } else {
-      return console.log("No key match!");
-    };
-  };
+function init() {
+  // Create new div for each object in the array
+  for (let index in SOUND_TRACKS) {
+    let newDiv = document.createElement('div');
+
+    // Assign class and data class
+    newDiv.classList.add('key');
+
+    // add keycode string to kbd tags
+
+    newDiv.setAttribute('data-key', KEY_CODES[index]);
+
+    newDiv.innerHTML = `<kbd>${KEY_LETTERS[index]}</kbd></br><span>${NAME_OF_SOUNDS[index]}</span>`;
+
+    containerRoom.appendChild(newDiv);
+
+  }
 };
+
+// Check if key press is equal to the following keys in KEY_CODES
+// function checkKeyCode(pressAnyKey) {
+//   for (let aKeyCode of KEY_CODES) {
+//     if (pressAnyKey === KEY_CODES) {
+//       // what is the key?
+//       return console.log("True!");
+//     } else {
+//       return console.log("No key match!");
+//     };
+//   };
+// };
 
 // document.onkeypress = function(evt) {
 //     evt = evt || window.event;
@@ -46,70 +65,91 @@ function checkKeyCode(pressAnyKey) {
 //     alert(charStr);
 // };
 
-window.addEventListener('keydown',function(e) {
   // const targetAudio = document.querySelector(`.sound[data-key="${e.keyCode}"]`);
   // if (e == targetAudio) {
   //   checkKeyCode(e);
   // }
 
-  let evt = e || window.event;
-  let charCode = evt.keyCode || evt.which;
+function playSound(e) {
+  // let evt = e || window.event;
+  // let charCode = evt.keyCode || evt.which;
   // var charStr = String.fromCharCode(charCode);
 
-  switch (charCode) {
+const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  switch (e.keyCode) {
     case 65:
-        // do something
-        console.log("HI")
+        // play audio
+        airHornTrack.currentTime = 0;
+        airHornTrack.play();
+        key.classList.add('playing');
+        console.log(key);
         break;
     case 83:
-        // do something
+        clownHornTrack.currentTime = 0;
+        clownHornTrack.play();
+        key.classList.add('playing');
+        console.log("s");
         break;
     case 68:
-        // do something
+        fartTootTrack.currentTime = 0;
+        fartTootTrack.play();
+        key.classList.add('playing');
+        console.log("d");
         break;
     case 70:
-        // do something
+        nikkoNikkoNiiTrack.currentTime = 0;
+        nikkoNikkoNiiTrack.play();
+        key.classList.add('playing');
+        console.log("f");
         break;
     case 71:
-        // do something
+        nyanCatTrack.currentTime = 0;
+        nyanCatTrack.play();
+        key.classList.add('playing');
+        console.log("g");
         break;
     case 72:
-        // do something
+        punchlineDrumTrack.currentTime = 0;
+        punchlineDrumTrack.play();
+        key.classList.add('playing');
+        console.log("h");
         break;
     case 74:
-        // do something
+        cackleTrack.currentTime = 0;
+        cackleTrack.play();
+        key.classList.add('playing');
+        console.log("j");
         break;
     case 75:
-        // do something
+        sexyHelloTrack.currentTime = 0;
+        sexyHelloTrack.play();
+        key.classList.add('playing');
+        console.log("k");
         break;
     case 76:
-        // do something
+        wooshTrack.currentTime = 0;
+        wooshTrack.play();
+        key.classList.add('playing');
+        console.log("l");
         break;
-
     default:
         return;
   }
-});
+}
 
-function init() {
-  // Create new div for each object in the array
-  for (let index in SOUND_TRACKS) {
-    let newDiv = document.createElement('div');
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return;
+  // console.log(e.propertyName);
+  this.classList.remove('playing');
+}
 
-    // Assign class and data class
-    newDiv.classList.add('sound');
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
 
-    // add keycode string to kbd tags
+window.addEventListener('keydown', playSound);
 
-    newDiv.setAttribute('key-data', KEY_CODES[index]);
 
-    newDiv.innerHTML = `<kbd>${KEY_LETTERS[index]}</kbd>`;
-    newDiv.innerHTML = `<span>${NAME_OF_SOUNDS[index]}</span>`;
 
-    containerRoom.appendChild(newDiv);
-
-  }
-};
 
 init();
-checkKeyCode();
